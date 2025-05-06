e7725f5d4aa8728e87154543bdab993e3699bda5
Gayatri-stack123 <gayatrichikkodi80@gmail.com>
2025-05-06 11:36:59 +0530

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,64 +1,22 @@
-
-
-import java.util.ArrayList;
-import java.util.List;
-public class Course {
-    String courseName;
-    int marksObtained;
-
-    // Constructor
-    public Course(String courseName, int marksObtained) {
-        this.courseName = courseName;
-        this.marksObtained = marksObtained;
-    }
-
-    // Display course details
-    public void displayCourse() {
-        System.out.println("Course: " + courseName + " | Marks: " + marksObtained);
-    }
-}
-
-class Semester {
-    int semesterNumber;
-    List<Course> courses;
-
-    // Constructor
-    public Semester(int semesterNumber) {
-        this.semesterNumber = semesterNumber;
-        this.courses = new ArrayList<>();
-    }
-
-    // Add a course to the semester
-    public void addCourse(String courseName, int marksObtained) {
-        courses.add(new Course(courseName, marksObtained));
-    }
-
-    // Display semester details
-    public void displaySemester() {
-        System.out.println("Semester " + semesterNumber + ":");
-        for (Course course : courses) {
-            course.displayCourse();
-        }
-        System.out.println("---------------------------");
-    }
-
-
+public class StudentReport {
     public static void main(String[] args) {
-        // Create semesters
-        Semester semester1 = new Semester(1);
-        semester1.addCourse("Mathematics", 85);
-        semester1.addCourse("Physics", 78);
-        semester1.addCourse("Chemistry", 80);
+        // Semester 1 courses and marks
+        String[] semester1Courses = {"Mathematics", "Physics", "Chemistry"};
+        int[] semester1Marks = {85, 78, 80};
 
-        Semester semester2 = new Semester(2);
-        semester2.addCourse("Computer Science", 90);
-        semester2.addCourse("English", 88);
-        semester2.addCourse("Economics", 82);
+        // Semester 2 courses and marks
+        String[] semester2Courses = {"Computer Science", "English", "Economics"};
+        int[] semester2Marks = {90, 88, 82};
 
         // Display semester details
-        semester1.displaySemester();
-        semester2.displaySemester();
+        System.out.println("Semester 1:");
+        for (int i = 0; i < semester1Courses.length; i++) {
+            System.out.println("Course: " + semester1Courses[i] + " | Marks: " + semester1Marks[i]);
+        }
+
+        System.out.println("\nSemester 2:");
+        for (int i = 0; i < semester2Courses.length; i++) {
+            System.out.println("Course: " + semester2Courses[i] + " | Marks: " + semester2Marks[i]);
+        }
     }
 }
-
-
